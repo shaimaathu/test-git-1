4c0fb6f4d4b3aeb949c18a2459c2032fd6363904
shaimaa <shaimaathu@MacBook-Air-alkhas-b-Abdullah.local>
2024-02-04 14:26:15 +0300

    shaimaa-althubaiti

---
+++ bin/test11.dart
@@ -0,0 +1,6 @@
+import 'package:test11/test11.dart' as test11;
+
+void main(List<String> arguments) {
+  final x = 5+5;
+  print(x);
+}
---
+++ test/test11_test.dart
@@ -0,0 +1,8 @@
+import 'package:test11/test11.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
